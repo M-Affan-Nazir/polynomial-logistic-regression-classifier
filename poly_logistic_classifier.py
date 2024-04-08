33005a7e4b1b411884ff111d61b8a0b25a5a35f4
M Affan Nazir <m.affann80@hotmail.com>
2024-04-07 18:58:48 -0600

    Complete 1

--- poly_logistic_classifier.py
+++ poly_logistic_classifier.py
@@ -1,9 +1,95 @@
 from sklearn.preprocessing import PolynomialFeatures
+import random
+import math
 import numpy as np
 import os
 
 
 
 class Polynomial_Classifier:
-     def __init__(self):
-          print("Hello")
+
+     def __init__(self, order):
+          self.order = order
+          self.weightInitialized = False
+          self.modelName = ""
+     
+     def name(self, name):
+        self.modelName = name
+
+     def _getFeatureVector(self,data = []):
+        data = [data]
+        poly = PolynomialFeatures(self.order)
+        features = poly.fit_transform(data)
+        return features[0]
+
+     def _convertToFeatureMatrix(self,x):
+        matrix = []
+        for i in x:
+            matrix.append(self._getFeatureVector(i))
+        return matrix
+     
+     def train(self, trainingData=(), validationData=(), epochs=0, batchSize=1, stepSize=0.01):
+        xTrain =  np.array(self._convertToFeatureMatrix(trainingData[0]))
+        xVal = np.array(self._convertToFeatureMatrix(validationData[0]))
+        yTrain = np.array(trainingData[1])
+        yVal = np.array(validationData[1])
+        self.batchSize = batchSize
+        self.features = len(xTrain[0])
+        if(self.weightInitialized == False):
+            self.w = np.random.rand(self.features)*0.01
+            self.weightInitialized = True
+        for i in range(epochs):
+            indices = list(range(len(xVal)))
+            random.shuffle(indices)
+            xVal_shuffled = [xVal[i] for i in indices]
+            yVal_shuffled = [yVal[i] for i in indices]
+            for j in range(0,len(xVal_shuffled),batchSize):
+                batchX = xVal_shuffled[j:j+batchSize]
+                batchY = yVal_shuffled[j:j+batchSize]
+                self._epoch(batchX,batchY, stepSize)
+            loss = self._cost(xTrain,yTrain)
+            valLoss = self._valLoss(xVal,yVal)
+            print("Epoch " + str(i+1) + ": | loss = " + str(self._round(loss,3)) + " validation loss = " + str(self._round(valLoss,3)))
+        
+
+
+     def _epoch(self, x, y, stepSize):
+        g = self._grad(x,y)
+        self.w = self.w - stepSize * g
+
+     def _grad(self,x,y):
+        p = self._predict(x)
+        diff = p - y
+        g = np.dot(diff.T,x)
+        g = g / len(p)
+        return g
+
+
+     def _predict(self, x):
+        return self._sigmoid(np.dot(x,self.w))
+     
+     def _cost(self,x,y):
+        p = self._predict(x)
+        #Categorial cross entropy
+        loss = 0
+        for i in range(len(p)):
+            loss += -y[i]*math.log(p[i]) - (1 - y[i])*math.log(1-p[i])   #p is a vector of prediction. We need the average cost; so we sum all predictions, (and coresponding y)
+        loss = loss/len(p)
+        return loss
+
+
+     def _valLoss(self, x,y):
+        return self._cost(x,y)
+     
+     def _sigmoid(self, x):
+        sig = []
+        for i in range(len(x)):
+            sig.append(1 / (1 + math.exp(-x[i])))
+        return np.array(sig)
+     
+     def _round(self, num, sig_figs):
+        if num == 0:
+            return 0
+        else:
+            return round(num, sig_figs - int(np.floor(np.log10(abs(num)))) - 1)
+    
