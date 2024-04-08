4c7c86616812586051df69920b3b5d68fa65d1b9
M Affan Nazir <m.affann80@hotmail.com>
2024-04-07 19:23:54 -0600

    Added MinMax min/max var extraction

--- poly_logistic_classifier.py
+++ poly_logistic_classifier.py
@@ -8,9 +8,10 @@ import os
 
 class Polynomial_Classifier:
 
-     def __init__(self, order):
+     def __init__(self, order, MinMaxObject = None):
           self.order = order
           self.weightInitialized = False
+          self.minmax = MinMaxObject
           self.modelName = ""
      
      def name(self, name):
@@ -50,8 +51,6 @@ class Polynomial_Classifier:
             loss = self._cost(xTrain,yTrain)
             valLoss = self._valLoss(xVal,yVal)
             print("Epoch " + str(i+1) + ": | loss = " + str(self._round(loss,3)) + " validation loss = " + str(self._round(valLoss,3)))
-        
-
 
      def _epoch(self, x, y, stepSize):
         g = self._grad(x,y)
@@ -64,7 +63,6 @@ class Polynomial_Classifier:
         g = g / len(p)
         return g
 
-
      def _predict(self, x):
         return self._sigmoid(np.dot(x,self.w))
      
@@ -76,7 +74,6 @@ class Polynomial_Classifier:
             loss += -y[i]*math.log(p[i]) - (1 - y[i])*math.log(1-p[i])   #p is a vector of prediction. We need the average cost; so we sum all predictions, (and coresponding y)
         loss = loss/len(p)
         return loss
-
 
      def _valLoss(self, x,y):
         return self._cost(x,y)
@@ -93,3 +90,25 @@ class Polynomial_Classifier:
         else:
             return round(num, sig_figs - int(np.floor(np.log10(abs(num)))) - 1)
     
+     def save(self, path=None):
+        if self.modelName != "":
+            if path is None:
+                path = os.getcwd()
+            if not os.path.isdir(path):
+                print(f"The directory {path} does not exist.")
+                return
+            if self.minmax != None:
+                min = self.minmax.data_min_ #An array, since num_weights > 1
+                max = self.minmax.data_max_
+
+            file_path = os.path.join(path, self.modelName + ".prf")
+
+            w_str = str(self.w.tolist())
+            try:
+                with open(file_path, 'w') as f:
+                    f.write(w_str)
+            except IOError as e:
+                print(f"An error occurred: {e}")
+        else:
+            print()
+            raise Exception("Save Failed: Model name required in order to export weights")
